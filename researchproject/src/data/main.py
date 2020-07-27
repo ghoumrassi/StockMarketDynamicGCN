37d2df1ea1e647d64438ebfb752bc739fe80d242
ghoumrassi <38295075+ghoumrassi@users.noreply.github.com>
2020-07-27 11:08:37 +0300

    Main finished, began work on torch datasets.py. Need to format stock files to h5 using make_h5.py

--- researchproject/src/data/main.py
+++ researchproject/src/data/main.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import pickle
 
-from researchproject.src import WD_OUTPUT, NYT_ENTITY_COUNT_MAP, FM_OUTPUT
-from researchproject.src.data import format_nytimes, get_wikidata, get_tiingo, fuzzy_match
+from src import WD_OUTPUT, NYT_OUTPUT, FM_OUTPUT
+from src.data import format_nytimes, get_wikidata, get_tiingo, fuzzy_match
 
 
 def main(download=False):
     # Creates counts file for all organisations in nytimes folder
-    # format_nytimes.get_entity_list(start_date='01/01/2010')
+    start_date = '01/01/2010'
+    # format_nytimes.get_entity_list(start_date=start_date)
+
 
     if download:
         # Creates wikidata tables
@@ -32,7 +34,7 @@ def main(download=False):
 
     # Fuzzy match nytimes with wikidata
     print("Creating fuzzy matching files...")
-    with open(NYT_ENTITY_COUNT_MAP, 'rb') as fn:
+    with open((NYT_OUTPUT / 'entities.p'), 'rb') as fn:
         nyt_entity_count = pickle.load(fn)
 
     nyt_names = list(nyt_entity_count.keys())
@@ -40,10 +42,20 @@ def main(download=False):
     lse_names = lse_subsidiaries['altLabel'].astype(str).unique()
 
     # Make nasdaq match output
-    fuzzy_match.make_fm_mapping(nyt_names, nasdaq_names, 'nasdaq_mapper.p')
+    fuzzy_match.make_fm_mapping(nyt_names, nasdaq_names, 'nasdaq')
 
     # Make lse match output
-    fuzzy_match.make_fm_mapping(nyt_names, lse_names, 'lse_mapper.p')
+    fuzzy_match.make_fm_mapping(nyt_names, lse_names, 'lse')
+
+    # Make nasdaq nyt summaries
+    with open((FM_OUTPUT / 'nasdaq_mapper.p'), 'rb') as fn:
+        mapper = pickle.load(fn)
+    format_nytimes.make_summaries(mapper, 'nasdaq_summaries.csv', start_date=start_date)
+
+    # Make nasdaq nyt summaries
+    with open((FM_OUTPUT / 'lse_mapper.p'), 'rb') as fn:
+        mapper = pickle.load(fn)
+    format_nytimes.make_summaries(mapper, 'lse_summaries.csv', start_date=start_date)
 
     print("DONE!")
 
