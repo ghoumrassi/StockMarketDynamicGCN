643c3859bc2be13bc0871c18f589bdeb2275e657
ghoumrassi <38295075+ghoumrassi@users.noreply.github.com>
2020-08-03 10:34:43 +0100

    Returns are now the sum of all returns to period, not just period

--- researchproject/src/data/datasets.py
+++ researchproject/src/data/datasets.py
@@ -42,10 +42,13 @@ class CompanyStockGraphDataset(Dataset):
             else:
                 additional_col_str = ""
             self.ticker_hist_query = q.format(additional_columns=additional_col_str)
-            self.ticker_future_query = q.format(additional_columns="")
+
+        with open((SQL_QUERIES / 'returns_future.q'), 'r') as f:
+            self.ticker_future_query = f.read()
 
         with open((SQL_QUERIES / 'get_distinct_dates.q'), 'r') as f:
             self.distinct_dates_query = f.read()
+
         self.c.execute(self.distinct_dates_query, (self.start_date, self.end_date))
         self.idx_date_map = {i: str(int(date[0])) for i, date in enumerate(self.c.fetchall())}
         self.date_idx_map = {str(int(date)): i for i, date in self.idx_date_map.items()}
@@ -61,22 +64,53 @@ class CompanyStockGraphDataset(Dataset):
         return len(self.idx_date_map) - (self.window_size + self.predict_periods)
 
     def __getitem__(self, idx):
-        X = torch.zeros(
-            (self.window_size, len(self.ticker_idx_map), len(self.features)+1),
-            device=self.device)
-        y = torch.zeros((len(self.ticker_idx_map),), device=self.device)
-        A = torch.zeros(
-            (self.window_size, len(self.ticker_idx_map), len(self.ticker_idx_map)),
-            device=self.device
-        )
-        k = torch.zeros((self.window_size, ), device=self.device)
-
         start_date = self.idx_date_map[idx]
         current_date = self.idx_date_map[idx + self.window_size]
         end_date = self.idx_date_map[idx + (self.window_size + self.predict_periods)]
 
-        # Get A
-        self.c.execute(self.pair_count_query, (start_date, current_date))
+        A = self.get_A(idx, start_date, current_date)
+        X = self.get_X(idx, start_date, current_date)
+        k = self.get_k()
+        y = self.get_y(current_date, end_date)
+
+        return A, X, k, y
+
+    def get_X(self, idx, start, current):
+        X = torch.zeros(
+            (self.window_size, len(self.ticker_idx_map), len(self.features)+1),
+            device=self.device)
+        self.c.execute(self.ticker_hist_query, (start, current))
+        results = self.c.fetchall()
+        for date, ticker, returns, *args in results:
+            if not returns:
+                continue
+            date_idx = self.date_idx_map[str(int(date))] - idx
+            ticker_idx = self.ticker_idx_map[ticker]
+            X[date_idx, ticker_idx, :] = torch.tensor([returns] + args)
+        return X
+
+    def get_y(self, current, end):
+        y = torch.zeros((len(self.ticker_idx_map),), device=self.device)
+        self.c.execute(self.ticker_future_query, (current, end))
+        results = self.c.fetchall()
+        for _, ticker, returns in results:
+            if not returns:
+                continue
+            ticker_idx = self.ticker_idx_map[ticker]
+            if returns < -self.returns_threshold:
+                y[ticker_idx] = 0
+            elif returns > self.returns_threshold:
+                y[ticker_idx] = 2
+            else:
+                y[ticker_idx] = 1
+        return y
+
+    def get_A(self, idx, start, current):
+        A = torch.zeros(
+            (self.window_size, len(self.ticker_idx_map), len(self.ticker_idx_map)),
+            device=self.device
+        )
+        self.c.execute(self.pair_count_query, (start, current))
         results = self.c.fetchall()
         for date, a, b, count in results:
             # TODO: Extremely messy solution: please fix.
@@ -93,38 +127,12 @@ class CompanyStockGraphDataset(Dataset):
                 d_i = self.date_idx_map[str(int(new_date))] - idx
             A[d_i, a_j, b_j] += count
             A[d_i, b_j, a_j] += count
+        return A
 
-        # Get X
-        self.c.execute(self.ticker_hist_query, (start_date, current_date))
-        results = self.c.fetchall()
-        for date, ticker, returns, *args in results:
-            if not returns:
-                continue
-            date_idx = self.date_idx_map[str(int(date))] - idx
-            ticker_idx = self.ticker_idx_map[ticker]
-            X[date_idx, ticker_idx, :] = torch.tensor([returns] + args)
-
-        # Get y
-        self.c.execute(self.ticker_future_query, (end_date, end_date))
-        results = self.c.fetchall()
-        for _, ticker, returns in results:
-            if not returns:
-                continue
-            ticker_idx = self.ticker_idx_map[ticker]
-            # oh_vector = torch.zeros(3, device=self.device)
-            if returns < -self.returns_threshold:
-                # oh_vector[0] = 1
-                y[ticker_idx] = 0
-            elif returns > self.returns_threshold:
-                # oh_vector[2] = 1
-                y[ticker_idx] = 2
-            else:
-                # oh_vector[1] = 1
-                y[ticker_idx] = 1
-        # Get k
+    def get_k(self):
+        k = torch.zeros((self.window_size,), device=self.device)
         k = k.fill_(len(self.ticker_idx_map))
-
-        return A, X, k, y
+        return k
 
 
 if __name__ == "__main__":
